259724b41c62da3d6e7503b1d46417603984179e
thxnk <grudin.svs@gmail.com>
2020-10-08 18:35:07 +0000

    Finally

---
+++ Solution/task2.md
@@ -0,0 +1,7 @@
+task2.1:  
+![](https://i.ibb.co/Jv2BphW/task2-1.png)
+
+task2.2:  
+![](https://i.ibb.co/Sr0jSBS/task2-2.png)
+Додав скріншот виконання додаткового завдання на 5:
+![](https://i.ibb.co/3ztkf1S/19.png)
---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+  String quote = "Too many of us are not living our dreams because we are living our fears.";
+  String NameOfAuthor = "Les Brown"; 
+  public void display() {
+    System.out.println(quote + " (" + NameOfAuthor + ")");
+  }
+} 
